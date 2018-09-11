efe8c1ac3de7181f4643e1a128b5561205a4de08
wilian caamal <wiliancaamal@hotmail.com>
2018-09-11 16:32:38 -0500

    Add Models
    Agregacion de modelos con entity framework

---
+++ CapaDatos/Usuario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+namespace CapaDatos
+{
+    public class Usuario
+    {
+        BibliotecaEntities db;
+
+        public Usuario()
+        {
+           db = new BibliotecaEntities();
+        }
+
+        public List<Usuarios> GetUsuarios()
+        {
+            return db.Usuarios.ToList();
+        }
+    }
+}
